f85223108f746daa581520963283a6936fcf3b09
STDSuperman <2750556766@qq.com>
2021-03-09 23:08:59 +0800

    feat: 添加组件插槽

--- vue-vite-app/src/components/Foo.vue
+++ vue-vite-app/src/components/Foo.vue
@@ -2,15 +2,10 @@
     <div class="foo">
         <p>这里是Foo</p>
         <div id="vue2Wrapper"></div>
-        <vue2-component>
-            <div>{{text}}</div>
-        </vue2-component>
     </div>
 </template>
 
 <script>
-import { vue2ToVue3 } from '../utils/index';
-import Vue2Component from './vue2.component.vue';
 import { ref } from 'vue';
 export default {
     name: 'Foo',
@@ -18,9 +13,6 @@ export default {
         return {
             text: ref('测试哇')
         }
-    },
-    components: {
-       'vue2-component': vue2ToVue3(Vue2Component, 'vue2Wrapper')
     }
 }
 </script>
