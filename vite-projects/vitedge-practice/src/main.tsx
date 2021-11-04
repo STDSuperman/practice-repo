145638b3b7f0180c1ffb294844d91ab4f2ce3e72
STDSuperman <2750556766@qq.com>
2021-11-04 23:30:23 +0800

    :fire: del: 移除vitedge示例项目

--- vite-projects/vitedge-practice/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './index.css'
-import App from './App'
-import vitedge from 'vitedge'
-
-export default vitedge(App, {
-  routes: []
-}, ctx => {})
--- vite-projects/vitedge-practice/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vitedgePlugin from 'vitedge/plugin.js'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vitedgePlugin(), react()]
-})
