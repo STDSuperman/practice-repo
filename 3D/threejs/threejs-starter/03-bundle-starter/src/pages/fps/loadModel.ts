5d026e40270ca112353c2dc4a81d7c7a2003250f
STDSuperman <2750556766@qq.com>
2022-01-23 22:11:28 +0800

    chore: 忽略本地模型文件

--- 3D/threejs/threejs-starter/03-bundle-starter/src/pages/fps/loadModel.ts
+++ 3D/threejs/threejs-starter/03-bundle-starter/src/pages/fps/loadModel.ts
@@ -4,16 +4,21 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
+// 如果是本地调试就从本地拿，节省流量 QAQ
+const sourceLoadCommonPath = window.location.hostname === 'localhost'
+  ? window.origin
+  : 'https://threejs-models-1257398419.cos.ap-nanjing.myqcloud.com'
+
 export const initCityModel = (
   scene: THREE.Scene | THREE.Group,
   camera: THREE.Camera
 ) => {
   const mtlLoader = new MTLLoader()
-  mtlLoader.setPath('/models/city-fps/')
+  mtlLoader.setPath(`${sourceLoadCommonPath}/models/city-fps/`)
   mtlLoader.load('city.mtl', material => {
     const objLoader = new OBJLoader()
     objLoader.setMaterials(material)
-    objLoader.setPath('/models/city-fps/')
+    objLoader.setPath(`${sourceLoadCommonPath}/models/city-fps/`)
     objLoader.load('city.obj', object => {
       //设置颜色的取值范围
       const scale = window.chroma.scale(['yellow', '008ae5']);
@@ -26,7 +31,7 @@ export const initCityModel = (
 }
 
 export const loadTreeHouse = (scene: THREE.Scene | THREE.Group) => {
-  const loader = new GLTFLoader().setPath('/models/kgirls01/' );
+  const loader = new GLTFLoader().setPath(`${sourceLoadCommonPath}/models/kgirls01/` );
   loader.load( 'scene.gltf', ( gltf ) => {
     console.log(gltf)
     gltf.scene.scale.x = 10000;
@@ -36,11 +41,11 @@ export const loadTreeHouse = (scene: THREE.Scene | THREE.Group) => {
 
 export const loadCityObj = (scene: THREE.Scene | THREE.Group) => {
   const mtlLoader = new MTLLoader()
-  mtlLoader.setPath('/models/city-scene-tokyo/source/Tokyo/')
+  mtlLoader.setPath(`${sourceLoadCommonPath}/models/city-scene-tokyo/source/Tokyo/`)
   mtlLoader.load('city.mtl', material => {
     const objLoader = new OBJLoader()
     objLoader.setMaterials(material)
-    objLoader.setPath('/models/city-scene-tokyo/source/Tokyo/')
+    objLoader.setPath(`${sourceLoadCommonPath}/models/city-scene-tokyo/source/Tokyo/`)
     objLoader.load('city.obj', object => {
       //设置颜色的取值范围
       const scale = window.chroma.scale(['yellow', '008ae5']);
@@ -63,8 +68,8 @@ export const loadPersonSkin = async (
   const loader = new FBXLoader();//创建一个FBX加载器
   const modelSet = (): Promise<VanguardModelLoadRes> => {
     return new Promise((resolve) => {
-      loader.load("/models/person-skin/remy.fbx", function(object) {
-        loader.load("/models/person-skin/walk.fbx", function(samba) {
+      loader.load(`${sourceLoadCommonPath}/models/person-skin/remy.fbx`, function(object) {
+        loader.load(`${sourceLoadCommonPath}/models/person-skin/walk.fbx`, function(samba) {
           object.scale.set(.1, .1, .1);
           const mixer = new THREE.AnimationMixer(object);
           let animationAction = mixer.clipAction(samba.animations[0]);
@@ -97,7 +102,7 @@ export const loadUSACity = (
   scene: THREE.Scene | THREE.Group,
   camera: THREE.Camera
 ) => {
-  const loader = new GLTFLoader().setPath('/models/sing_sing_prison_ossining_new_york_usa/' );
+  const loader = new GLTFLoader().setPath(`${sourceLoadCommonPath}/models/sing_sing_prison_ossining_new_york_usa/` );
   loader.load( 'scene.gltf', ( gltf ) => {
     gltf.scene.scale.set(50, 50, 50)
     scene.add( gltf.scene );
@@ -108,7 +113,7 @@ export const loadLowPolyCityPack = (
   scene: THREE.Scene | THREE.Group,
   camera: THREE.Camera
 ) => {
-  const loader = new GLTFLoader().setPath('/models/low-poly-city-pack/' );
+  const loader = new GLTFLoader().setPath(`${sourceLoadCommonPath}/models/low-poly-city-pack/` );
   loader.load( 'scene.gltf', ( gltf ) => {
     gltf.scene.scale.set(50, 50, 50)
     scene.add( gltf.scene );
@@ -119,7 +124,7 @@ export const loadDreamHouse = (
   scene: THREE.Scene | THREE.Group,
   camera: THREE.Camera
 ) => {
-  const loader = new GLTFLoader().setPath('/models/the_dream_house/' );
+  const loader = new GLTFLoader().setPath(`${sourceLoadCommonPath}/models/the_dream_house/` );
   loader.load( 'scene.gltf', ( gltf ) => {
     gltf.scene.scale.set(40, 40, 40)
     scene.add( gltf.scene );
@@ -130,7 +135,7 @@ export const loadGrass = (
   scene: THREE.Scene | THREE.Group,
   camera: THREE.Camera
 ) => {
-  const loader = new GLTFLoader().setPath('/models/grass_usd/' );
+  const loader = new GLTFLoader().setPath(`${sourceLoadCommonPath}/models/grass_usd/` );
   loader.load( 'scene.gltf', ( gltf ) => {
     gltf.scene.scale.set(100, 1, 100)
     scene.add( gltf.scene );
@@ -144,7 +149,7 @@ export const loadMedievalFantasyBook = async (
   const loader = new FBXLoader();//创建一个FBX加载器
   const modelSet = (): Promise<VanguardModelLoadRes> => {
     return new Promise((resolve) => {
-      loader.load("/models/medieval-fantasy-book/textures/TEST2.fbx", function(object) {
+      loader.load(`${sourceLoadCommonPath}/models/medieval-fantasy-book/textures/TEST2.fbx`, function(object) {
         object.scale.set(20, 20, 20);
         const mixer = new THREE.AnimationMixer(object);
         let animationAction = mixer.clipAction(object.animations[0]);
